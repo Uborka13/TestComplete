29c1ef144218636e117423780d0a3f0efb9e81aa
Uborka13 <dr.dave94@gmail.com>
2017-05-15 15:42:03 +0200

    commit suicide

--- gittertester/TestProject1/Script/Unit1.py
+++ gittertester/TestProject1/Script/Unit1.py
@@ -1,14 +1,24 @@
 ﻿def Func():
-  print(sum(x for x in range(1000))
+  print(sum(x for x in range(1000)))
   
 
 print(Func())
 def valami_function():
+  some = True
   return 0
   
   
 def valami_function2():
-  return not True
+  love = True
+  love is False
+  False is True
+  love is True
+  return not love
 
 
-Select lofasz
+def change_love():
+  love = False
+  False is False
+  True is True
+  love is True
+  False is love
